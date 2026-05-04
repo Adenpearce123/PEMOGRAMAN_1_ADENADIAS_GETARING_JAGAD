870d616327ea41a1c8d3ff4fe3d22d7687086256
Adenpearce123 <143508040+Adenpearce123@users.noreply.github.com>
2026-05-04 18:36:41 +0700

    Add files via upload

---
+++ pertemuan1.java
@@ -0,0 +1,11 @@
+package latihan;
+
+public class pertemuan1 {
+    public static void main(String[] args) {
+
+        // Menampilkan teks ke layar
+        System.out.println("Hello, World!");
+        System.out.println("Pertemuan 1 - Pengenalan Java");
+
+    }
+}
